60db1f4a43c0e5e149df2905dc02c101e534e689
Elijah Miernicki <miernickielijah@gmail.com>
2021-05-13 14:11:19 -0500

    updated live date, created bootstrap table

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # barbarian
-this is a scheduler 
+This is a scheduler that allows users to save written notes via time to thier local computer. 
+
+
+## Image
+![screenshot]("\assets\screenshot.png")
+
+### Specifications
+
+* User on a time they can enter in information 
+
+* When a user clicks on the save button it saves the entered information to the local file
+
+* When a user reloads the page the information is pulled from the local file
+
+* When user clicks on a file it turns color, when time is > then left column it greys out.
+
+## 💡 Notes
+https://getbootstrap.com/docs/4.0/components/alerts/
+https://stackoverflow.com/questions/62271505/how-can-i-set-different-colors-for-time-blocks-in-a-calendar-or-day-planner-usin
+https://moment.github.io/luxon/docs/class/src/datetime.js~DateTime.html#instance-method-toLocaleString
+https://www.youtube.com/watch?v=vAuUzEwTbck
+
+
--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-console.log(luxon.DateTime.now().toString())
+var now = luxon.DateTime.now().toLocaleString({ weekday: 'short', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit' });
 
+
+
+// change date format to be better looking "cccc, LLL dd, yyyy, hh:mm:ss" //
 setInterval(function () {
-    luxon.DateTime.now();
-    var now = luxon.DateTime.now();
-    luxon.DateTime.now().toString();
+    var now = luxon.DateTime.now().toLocaleString({ weekday: 'long', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' });
+    luxon.DateTime.local().toString();
+    luxon.DateTime.local();
     $('#currentDay').text(now);
-    console.log(now)
 }, 1000)
 
-// change date format to be better looking //
 
+//data attr based on the time in first column// for save button and for text area //
 
+//data attr on both text area and button //
 
+//key value pair [time : ""] saved to local storage //
+
+//retrieve all key value pairs from local storage and wrtie to DOM //
