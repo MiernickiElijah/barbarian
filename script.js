ac3476cf7accf628efc4b78a4c7d13077ad6a863
Elijah Miernicki <miernickielijah@gmail.com>
2021-05-14 10:43:25 -0500

    save to local drive with all btns

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
 var now = luxon.DateTime.now().toLocaleString({ weekday: 'short', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit' });
-var userInputs = [
-    { time: 7, input: "" },
-    { time: 8, input: "" },
-    { time: 9, input: "" },
-    { time: 10, input: "" },
-    { time: 11, input: "" },
-    { time: 12, input: "" },
-    { time: 1, input: "" },
-    { time: 2, input: "" },
-    { time: 3, input: "" },
-    { time: 4, input: "" },
-    { time: 5, input: "" },
-];
-
 //Live Date and Time for header//
 setInterval(function () {
     var now = luxon.DateTime.now().toLocaleString({ weekday: 'long', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' });
@@ -23,7 +9,7 @@ setInterval(function () {
 
 var milTime = luxon.DateTime.now().hour;
 //compare time to schedule change class for color//
-function colorClock() {
+function colorClock7() {
     if (milTime === 7) {
         $(".col.col-lg-8.future.textarea.description.7").addClass("present").removeClass("past future");
     } if (milTime > 7) {
@@ -32,40 +18,136 @@ function colorClock() {
         $(".col.col-lg-8.future.textarea.description.7").addClass("future").removeClass("present past");
     };
 };
-colorClock();
-console.log(milTime);
+function colorClock8() {
+    if (milTime === 8) {
+        $(".col.col-lg-8.future.textarea.description.8").addClass("present").removeClass("past future");
+    } if (milTime > 8) {
+        $(".col.col-lg-8.future.textarea.description.8").addClass("past").removeClass("present future");
+    } else {
+        $(".col.col-lg-8.future.textarea.description.8").addClass("future").removeClass("present past");
+    };
+};
+function colorClock9() {
+    if (milTime === 9) {
+        $(".col.col-lg-8.future.textarea.description.9").addClass("present").removeClass("past future");
+    } if (milTime > 9) {
+        $(".col.col-lg-8.future.textarea.description.9").addClass("past").removeClass("present future");
+    } else {
+        $(".col.col-lg-8.future.textarea.description.9").addClass("future").removeClass("present past");
+    };
+};
+function colorClock10() {
+    if (milTime === 10) {
+        $(".col.col-lg-8.future.textarea.description.10").addClass("present").removeClass("past future");
+    } if (milTime > 10) {
+        $(".col.col-lg-8.future.textarea.description.10").addClass("past").removeClass("present future");
+    } else {
+        $(".col.col-lg-8.future.textarea.description.10").addClass("future").removeClass("present past");
+    };
+};
+function colorClock11() {
+    if (milTime === 11) {
+        $(".col.col-lg-8.future.textarea.description.11").addClass("present").removeClass("past future");
+    } if (milTime > 11) {
+        $(".col.col-lg-8.future.textarea.description.11").addClass("past").removeClass("present future");
+    } else {
+        $(".col.col-lg-8.future.textarea.description.11").addClass("future").removeClass("present past");
+    };
+};
+function colorClock12() {
+    if (milTime === 12) {
+        $(".col.col-lg-8.future.textarea.description.12").addClass("present").removeClass("past future");
+    } if (milTime > 12) {
+        $(".col.col-lg-8.future.textarea.description.12").addClass("past").removeClass("present future");
+    } else {
+        $(".col.col-lg-8.future.textarea.description.12").addClass("future").removeClass("present past");
+    };
+};
+function colorClock13() {
+    if (milTime === 13) {
+        $(".col.col-lg-8.future.textarea.description.13").addClass("present").removeClass("past future");
+    } if (milTime > 13) {
+        $(".col.col-lg-8.future.textarea.description.13").addClass("past").removeClass("present future");
+    } else {
+        $(".col.col-lg-8.future.textarea.description.13").addClass("future").removeClass("present past");
+    };
+};
+function colorClock14() {
+    if (milTime === 14) {
+        $(".col.col-lg-8.future.textarea.description.14").addClass("present").removeClass("past future");
+    } if (milTime > 14) {
+        $(".col.col-lg-8.future.textarea.description.14").addClass("past").removeClass("present future");
+    } else {
+        $(".col.col-lg-8.future.textarea.description.14").addClass("future").removeClass("present past");
+    };
+};
+function colorClock15() {
+    if (milTime === 15) {
+        $(".col.col-lg-8.future.textarea.description.15").addClass("present").removeClass("past future");
+    } if (milTime > 15) {
+        $(".col.col-lg-8.future.textarea.description.15").addClass("past").removeClass("present future");
+    } else {
+        $(".col.col-lg-8.future.textarea.description.15").addClass("future").removeClass("present past");
+    };
+};
+function colorClock16() {
+    if (milTime === 16) {
+        $(".col.col-lg-8.future.textarea.description.16").addClass("present").removeClass("past future");
+    } if (milTime > 16) {
+        $(".col.col-lg-8.future.textarea.description.16").addClass("past").removeClass("present future");
+    } else {
+        $(".col.col-lg-8.future.textarea.description.16").addClass("future").removeClass("present past");
+    };
+};
+function colorClock17() {
+    if (milTime === 17) {
+        $(".col.col-lg-8.future.textarea.description.17").addClass("present").removeClass("past future");
+    } if (milTime > 17) {
+        $(".col.col-lg-8.future.textarea.description.17").addClass("past").removeClass("present future");
+    } else {
+        $(".col.col-lg-8.future.textarea.description.17").addClass("future").removeClass("present past");
+    };
+};
+//run the clocks for each row//
+colorClock7();
+colorClock8();
+colorClock9();
+colorClock10();
+colorClock11();
+colorClock12();
+colorClock13();
+colorClock14();
+colorClock15();
+colorClock16();
+colorClock17();
 
-
-
-//get military time from luxon//
-/* if (current() < .time) then add future class
-
-
-if (now() > .time) then add past class
-if (now() === .time) then add present class */
-
-function compareTime() {
-}
-compareTime();
-
-
-//key value pair [time : ""] saved to local storage //
-$("#saveBtn").click(function (event) {
-    var userEvent = document.getElementById("description").value;
-    localStorage.setItem("time", userEvent);
-});
 
 //retrieve all key value pairs from local storage and wrtie to DOM //
 function loadEvents() {
-    var userEvent = localStorage.getItem("time");
-    document.getElementsById("description").value;
+    var userTextInputs = localStorage.getItem("userInputs");
+    function displayUserTextInputs(type, message) {
+        textarea.textContent = message;
+        textarea.setAttribute("userInputs", type)
+    }
 }
+loadEvents();
+
+
+
+//saved to local storage on click //
+var userTextInput = $(".textarea").textContent;
+var allContnet = $("#content")
+
+allContnet.on("click", ".saveBtn", function (event) {
+    event.preventDefault();
+    var userTextInput = document.querySelector("textarea").value;
+    localStorage.setItem("userInputs", userTextInput);
+});
+
 //get from local storage
 //make into JSON 
 //loop through properties 
 //week 4 activity 24 &2 5 
-
-
 
 //data attr based on the time in first column// for save button and for text area //
 
